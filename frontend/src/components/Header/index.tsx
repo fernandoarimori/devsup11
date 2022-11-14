62d8afa0a571af1f54ca930d42448915ce3dcc8a
Fernando Arimori <fernando.c.arimori@gmail.com>
2022-11-14 19:47:12 -0300

    Others components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logoHeader from '../../assets/img/logo-header.svg'
+import './styles.css'
+
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logoHeader} alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+        </p>
+      </div>
+    </header>
+  );
+}
+export default Header;
